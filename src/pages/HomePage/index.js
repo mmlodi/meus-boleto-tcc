a2deeef5ad2e0df1318db93ecc1b4feb5640aaf0
lodi <mischellodi@gmail.com>
2024-07-30 22:20:10 -0300

    primeiro commit

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import DataGridDemo from '../../components/MainTable';
+
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>MeusBoleto</h1>
+
+                <DataGridDemo/>
+            </div>
+    );
+
+};
+
+export default HomePage;
